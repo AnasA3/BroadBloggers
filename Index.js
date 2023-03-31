0de88c13a3ec0c46b9268379dbd7c206c473a63e
Anas <anas@gmail.com>
2023-03-31 16:22:47 -0400

    added

--- Index.js
+++ Index.js
@@ -4,6 +4,8 @@ var read = document.getElementById("Readbtn");
  function function1(){
     dropcontent.style.display = "block";
     read.style.display = "none"
+    read.alignitems = "center";
+    
 }
 function function2(){
     dropcontent.style.display = "none";
