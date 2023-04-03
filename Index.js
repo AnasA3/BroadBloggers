f751adf1e48e8f98fa3d492c848f821de8c525da
Anas <anas@gmail.com>
2023-04-02 22:31:07 -0400

    commit

--- Index.js
+++ Index.js
@@ -1,13 +1,52 @@
 var dropcontent = document.getElementById("Dropcontent");
 var read = document.getElementById("Readbtn");
+
+var dropcontent2 = document.getElementById("Dropcontent2");
+var read2 = document.getElementById("Readbtn2");
  
- function function1(){
-    dropcontent.style.display = "block";
+var dropcontent3 = document.getElementById("Dropcontent3");
+var read3 = document.getElementById("Readbtn3");
+
+var dropcontent4 = document.getElementById("Dropcontent4");
+var read4 = document.getElementById("Readbtn4");
+
+function foodbtn(){
+    dropcontent.style.display = "inline-block";
     read.style.display = "none"
     read.alignitems = "center";
     
 }
-function function2(){
+function foodbtn2(){
     dropcontent.style.display = "none";
-    read.style.display = "block";
+    read.style.display = "inline-block";
+}
+
+function healthbtn(){
+    dropcontent2.style.display = "inline-block";
+    read2.style.display = "none";
+    read.alignitems = "center";
+}
+function healthbtn2(){
+    dropcontent2.style.display = "none";
+    read2.style.display = "inline-block";
+}
+
+function bookbtn(){
+    dropcontent3.style.display = "inline-block";
+    read3.style.display = "none"
+    read3.alignitems = "center";
+}
+function bookbtn2(){
+    dropcontent3.style.display = "none";
+    read3.style.display = "inline-block";
+}
+
+function Sportbtn(){
+    dropcontent4.style.display = "inline-block";
+    read4.style.display = "none"
+    read4.alignitems = "center";
+}
+function Sportbtn2(){
+    dropcontent4.style.display = "none";
+    read4.style.display = "inline-block";
 }
