41fe0f80b6ae4bc02dddc49882fc92f4327e1769
Anas <anas@gmail.com>
2023-04-10 02:32:42 -0400

    commit

--- Index.js
+++ Index.js
@@ -1,13 +1,82 @@
-var dropcontent = document.getElementById("Dropcontent");
-var read = document.getElementById("Readbtn");
- 
- function function1(){
-    dropcontent.style.display = "block";
-    read.style.display = "none"
-    read.alignitems = "center";
-    
+/SECTION 1/
+
+const carousel = document.querySelector('.carousel');
+const carouselItems = carousel.querySelectorAll('.carousel-item');
+const indicators = document.querySelectorAll('.carousel-indicators li');
+
+let currentIndex = 0;
+
+function showSlide(index) {
+  carouselItems.forEach((item, i) => {
+    if (i === index) {
+      item.classList.add('active');
+    } else {
+      item.classList.remove('active');
+    }
+  });
 }
-function function2(){
-    dropcontent.style.display = "none";
-    read.style.display = "block";
+
+function showIndicator(index) {
+  indicators.forEach((indicator, i) => {
+    if (i === index) {
+      indicator.classList.add('active');
+    } else {
+      indicator.classList.remove('active');
+    }
+  });
 }
+
+indicators.forEach((indicator, i) => {
+  indicator.addEventListener('click', () => {
+    currentIndex = i;
+    showSlide(currentIndex);
+    showIndicator(currentIndex);
+  });
+});
+
+function nextSlide() {
+  currentIndex++;
+  if (currentIndex >= carouselItems.length) {
+    currentIndex = 0;
+  }
+  showSlide(currentIndex);
+  showIndicator(currentIndex);
+}
+
+function prevSlide() {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = carouselItems.length - 1;
+  }
+  showSlide(currentIndex);
+  showIndicator(currentIndex);
+}
+
+setInterval(nextSlide, 3000);
+
+
+/FUTURE DEPENDS SECTION/
+
+const infoSection = document.querySelector(".future");
+const hiddenElements = infoSection.querySelectorAll(".hidden");
+window.addEventListener("scroll", revealElements);
+
+function revealElements() {
+	const topOfSection = infoSection.offsetTop - window.innerHeight / 2;
+	if (window.scrollY > topOfSection) {
+		hiddenElements.forEach(element => element.classList.add("visible"));
+	} else {
+		hiddenElements.forEach(element => element.classList.remove("visible"));
+	}
+}
+
+/FOOD CONTENT ON CATEGORIES PAGE/
+
+function showFood() {
+  document.getElementById("food").style.display = "block";
+}
+
+function hideFood() {
+  document.getElementById("food").style.display = "none";
+}
+
